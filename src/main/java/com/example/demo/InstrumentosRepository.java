518a85a267d556c185e6e179a7edae9f03b437e3
BrandomKou57 <134289852+BrandomKou57@users.noreply.github.com>
2023-07-24 15:13:14 +0000

    cambios pom

--- src/main/java/com/example/demo/InstrumentosRepository.java
+++ src/main/java/com/example/demo/InstrumentosRepository.java
@@ -1,15 +1,15 @@
 package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
-// import org.springframework.data.repository.PagingAndSortingRepository;
-// import org.springframework.data.repository.query.Param;
-// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-// import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
-// @RepositoryRestResource(collectionResourceRel = "cursos", path = "cursos")
-public interface InstrumentosRepository extends CrudRepository<Instrumentos, Integer> {
+@RepositoryRestResource(collectionResourceRel = "instrumentos", path = "instrumentos")
+public interface InstrumentosRepository extends PagingAndSortingRepository<Instrumentos, Integer>,CrudRepository<Instrumentos, Integer> {
     
-   
+   List<Instrumentos> findByNombre(@Param("nombre") String nombre);
 
 }
 
