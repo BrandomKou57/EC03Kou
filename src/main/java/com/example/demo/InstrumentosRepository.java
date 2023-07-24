bf5666c88106bfcf6976aecb0553cdc81e466672
BrandomKou57 <134289852+BrandomKou57@users.noreply.github.com>
2023-07-24 14:43:46 +0000

    deploy en render

---
+++ src/main/java/com/example/demo/InstrumentosRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.PagingAndSortingRepository;
+// import org.springframework.data.repository.query.Param;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+// import java.util.List;
+
+// @RepositoryRestResource(collectionResourceRel = "cursos", path = "cursos")
+public interface InstrumentosRepository extends CrudRepository<Instrumentos, Integer> {
+    
+   
+
+}
+
+// InstrumentosRepository extends PagingAndSortingRepository<Curso, Integer>,
+//  List<Curso> findByNombre(@Param("nombre") String nombre);
